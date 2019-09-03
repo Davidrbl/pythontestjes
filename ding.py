5890ceb25c39e964d5714880ddc2c8cd5af2907e
David <38097012+Davidrbl@users.noreply.github.com>
2019-09-03 15:37:43 +0200

    Add files via upload
    Functies erin en bezig met in files saven

--- ding.py
+++ ding.py
@@ -1,14 +1,29 @@
-namen = {"David": "1", "Karsten": "2", "Douwe": "3"}
+dict = {"David": "1", "Karsten": "2", "Douwe": "3"}
+'''dict = open("dict.txt", "w")'''
 
+def delItemCheck(ding):
+    delitem = input("Welk item wilt u verwijderen? ")
+    del dict[delitem]
+
+def addItem():
+    newkey = input("Key: ")
+    newvalue = input("Value: ")
+
+    dict[newkey] = newvalue
+
+def viewAll():
+    for key, value in dict.items():
+        print(key, value)
 
 while True:
-    ding = input()
+    ding = input("Kies tussen de volgende opties:\n \nTyp de naam van uw key:\nZeg add om een item toe te voegen:\nTyp del om een item te verwijderen:\nTyp all om de hele dictionary te zien:\n")
     if ding == "quit":
         break
     elif ding == "add":
-        newkey = input("Key: ")
-        newvalue = input("Value: ")
-        namen[newkey] = newvalue
-        print(namen)
-        input()
-    print(namen[ding])
+        addItem()
+    elif ding == "del":
+        delItemCheck(ding)
+    elif ding == "all":
+        viewAll()
+    else:
+        print(dict[ding])
